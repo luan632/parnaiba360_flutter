a76926e2e28fc6ca4c7debbc8f2fe532e977299f
luan632 <holandaluan38@gmail.com>
2025-06-21 00:10:07 -0300

    quase pronto

--- lib/page/Open_Street_Map.dart
+++ lib/page/Open_Street_Map.dart
@@ -17,31 +17,79 @@ class OpenStreetMap extends StatefulWidget {
 class _OpenStreetMapState extends State<OpenStreetMap> {
   final MapController _mapController = MapController();
   final LatLng parnaibaLocation = const LatLng(-2.9038, -41.7767);
-  String selectedFilter = 'Todos';
+  String selectedFilter = 'Ponto Turístico'; // Alterado para um valor padrão diferente de 'Todos'
   final AuthService _authService = AuthMockService();
   late Future<List<PontosTuristicos>> futurePontos;
-  // Lista de pontos turísticos
+
   void initState() {
     super.initState();
-    futurePontos = ApiServices().getPontos(); 
+    futurePontos = ApiServices().getPontos();
   }
-  
+
   final List<Map<String, dynamic>> pontosTuristicos = [
-    {'nome': "Praça Mandu Ladino", 'lat': -2.902957, 'lng': -41.768434},
-    {'nome': "Parnaíba Shopping", 'lat': -2.909734, 'lng': -41.746951},
-    {'nome': "Praia Pedra do Sal", 'lat': -2.816892, 'lng': -41.728505},
-    {'nome': "Lagoa do Portinho", 'lat': -2.963750, 'lng': -41.683123},
+    {
+      'nome': "Praça Mandu Ladino",
+      'lat': -2.902870,
+      'lng': -41.768831,
+      'descricao': "A Praça Mandu Ladino é um espaço histórico da cidade.",
+      'endereco': "Rua Paul Harris - Nossa Sra. de Fátima, Parnaíba - PI, 64202-400"
+    },
+    {
+      'nome': "Parnaíba Shopping",
+      'lat': -2.909734,
+      'lng': -41.746951,
+      'descricao': "O Parnaíba Shopping é o único centro de compras, serviços e entretenimento da região do litoral do Piauí, localizado em Parnaíba.",
+      'endereco': "Av. São Sebastião, 3429 - Reis Veloso, Parnaíba - PI, 64204-035"
+    },
+    {
+      'nome': "Praia Pedra do Sal",
+      'lat': -2.805365,
+      'lng': -41.729110,
+      'descricao': "Praia famosa pela pesca artesanal e gastronomia local.",
+      'endereco': "Pedra do Sal, Parnaíba - PI"
+    },
+    {
+      'nome': "Lagoa do Portinho",
+      'lat': -2.931272,
+      'lng': -41.676872,
+      'descricao': "Área de lazer com lago natural e trilhas ecológicas mais também pela sua lenda que envolve amor, rivalidade tribal e a intervenção do deus Tupã, conta que a lagoa surgiu das lágrimas de Macyrajara, uma índia da tribo dos Tremembés, após a morte de seu amado Ubitã, guerreiro de uma tribo rival.",
+      'endereco': "Estrada Portinho, Parnaíba - PI"
+    },
   ];
 
-  // Lista de hotéis
   final List<Map<String, dynamic>> hoteis = [
-    {'nome': 'Citi Executivo Hotel', 'lat': -2.913962, 'lng': -41.753847},
-    {'nome': 'Hotel Portal dos Ventos', 'lat': -2.908528, 'lng': -41.752094},
+    {
+      'nome': 'Hotel Cívico',
+      'lat': -2.903465,
+      'lng': -41.773410,
+      'descricao': "Hotel moderno e bem localizado no centro comercial.",
+      'endereco': "Av. Chagas Rodrigues, 474 - Centro, Parnaíba - PI, 64200-490"
+    },
+    {
+      'nome': 'Hotel Delta',
+      'lat': -2.902006,
+      'lng': -41.779482,
+      'descricao': "Localizado próximo ao centro histórico da cidade o Porto das Barcas.",
+      'endereco': "Av. Pres. Getúlio Vargas, 268 - Centro, Parnaíba - PI, 64200-200"
+    },
+    
   ];
 
-  // Lista de restaurantes
   final List<Map<String, dynamic>> restaurantes = [
-    {'nome': 'Restaurante 1', 'lat': -2.910237, 'lng': -41.744985},
+    {
+      'nome': 'Restaurante Mangata',
+      'lat': -2.910322,
+      'lng': -41.744839,
+      'descricao': "Comida regional e pratos internacionais.",
+      'endereco': "Av. São Sebastião, 3900 - Frei Higino, Parnaíba - PI, 64207-005"
+    },
+    {
+      'nome': 'Restaurante Don Ladino',
+      'lat': -2.903190,
+      'lng': -41.768249,
+      'descricao': "Comida regional e pratos internacionais.",
+      'endereco': "Rua Padre Raimundo José Viêira, 378 - Nossa Sra. de Fátima, Parnaíba - PI, 64202-340"
+    },
   ];
 
   List<Map<String, dynamic>> _generateMarkers(
@@ -52,34 +100,63 @@ class _OpenStreetMapState extends State<OpenStreetMap> {
         'nome': location['nome'],
         'marker': Marker(
           point: LatLng(location['lat'], location['lng']),
-          builder: (_) => Icon(
-            type == 'Ponto Turístico' ? Icons.location_on : 
-            type == 'Hotel' ? Icons.hotel : Icons.restaurant,
-            color: color,
-          ),
           width: 30,
           height: 30,
           anchorPos: AnchorPos.align(AnchorAlign.top),
+          builder: (_) => GestureDetector(
+            onTap: () {
+              _showMarkerInfo(context, location);
+            },
+            child: Icon(
+              type == 'Ponto Turístico'
+                  ? Icons.location_on
+                  : type == 'Hotel'
+                      ? Icons.hotel
+                      : Icons.restaurant,
+              color: color,
+            ),
+          ),
         ),
       };
     }).toList();
   }
 
+  void _showMarkerInfo(BuildContext context, Map<String, dynamic> info) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(info['nome']),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Descrição: ${info['descricao']}"),
+            const SizedBox(height: 10),
+            Text("Endereço: ${info['endereco']}"),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: Navigator.of(context).pop,
+            child: const Text('Fechar'),
+          )
+        ],
+      ),
+    );
+  }
+
   List<Marker> getFilteredMarkers() {
     List<Map<String, dynamic>> allMarkers = [];
-    
+
     allMarkers.addAll(_generateMarkers(pontosTuristicos, 'Ponto Turístico', Colors.red));
     allMarkers.addAll(_generateMarkers(hoteis, 'Hotel', Colors.blue));
-    allMarkers.addAll(_generateMarkers(restaurantes, 'Restaurante', Colors.green));
+    allMarkers.addAll(_generateMarkers(restaurantes, 'Restaurante', Colors.orange));
 
-    if (selectedFilter == 'Todos') {
-      return allMarkers.map((m) => m['marker'] as Marker).toList();
-    } else {
-      return allMarkers
-          .where((marker) => marker['type'] == selectedFilter)
-          .map((m) => m['marker'] as Marker)
-          .toList();
-    }
+    // Removida a condição para 'Todos'
+    return allMarkers
+        .where((marker) => marker['type'] == selectedFilter)
+        .map((m) => m['marker'] as Marker)
+        .toList();
   }
 
   void _applyFilter(String? filter) {
@@ -133,25 +210,46 @@ class _OpenStreetMapState extends State<OpenStreetMap> {
           PopupMenuButton<String>(
             onSelected: _applyFilter,
             itemBuilder: (context) => [
-              const PopupMenuItem(
-                value: 'Todos',
-                child: Text('Todos'),
-              ),
+              // Removido o item 'Todos' do menu
               const PopupMenuItem(
                 value: 'Ponto Turístico',
-                child: Text('Pontos Turísticos'),
+                child: Row(
+                  children: [
+                    Icon(Icons.location_on, color: Colors.red),
+                    SizedBox(width: 10),
+                    Text('Pontos Turísticos'),
+                  ],
+                ),
               ),
               const PopupMenuItem(
                 value: 'Hotel',
-                child: Text('Hotéis'),
+                child: Row(
+                  children: [
+                    Icon(Icons.hotel, color: Colors.blue),
+                    SizedBox(width: 10),
+                    Text('Hotéis'),
+                  ],
+                ),
               ),
               const PopupMenuItem(
                 value: 'Restaurante',
-                child: Text('Restaurantes'),
+                child: Row(
+                  children: [
+                    Icon(Icons.restaurant, color: Colors.orange),
+                    SizedBox(width: 10),
+                    Text('Restaurantes'),
+                  ],
+                ),
               ),
               const PopupMenuItem(
                 value: 'Sair',
-                child: Text('Sair', style: TextStyle(color: Colors.red)),
+                child: Row(
+                  children: [
+                    Icon(Icons.logout, color: Colors.red),
+                    SizedBox(width: 10),
+                    Text('Sair', style: TextStyle(color: Colors.red)),
+                  ],
+                ),
               ),
             ],
           ),
@@ -169,7 +267,7 @@ class _OpenStreetMapState extends State<OpenStreetMap> {
             ),
             children: [
               TileLayer(
-                urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png", 
+                urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",   
               ),
               CurrentLocationLayer(
                 style: const LocationMarkerStyle(
